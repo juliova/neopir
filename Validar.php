764a13e89935c538e8085cf342a0f35adc9be5c6
julio villagran <ticogameboycr1@gmail.com>
2019-07-22 23:48:12 -0600

    Mayusculas

--- Validar.php
+++ Validar.php
@@ -2,9 +2,6 @@
 include 'Base.php';
 //Coneccion ala base de datos
 $conn = conectar();
-if($conn->connect_error){
-die("Conexion a la base fallida: " . $connect_error);
-}
 //Obtencion de los datos de los campos
 $cedula = $_POST['cedula3'];
 $token = $_POST['token'];
@@ -13,11 +10,11 @@ $token = $_POST['token'];
 //Llamado al procedimiento almacenado
 $sql = "CALL Validacion (".$cedula .", '".$token. "');";
 $conn->query($sql);
-if(mysqli_affected_rows($conn) >0){
+if($conn->affected_rows >0){
   echo "<script type='text/javascript'>alert('Validacion Correcta'); window.location.href='index.php';</script>";
   //header('Location:index.html');
 }else{
-  echo "<script type='text/javascript'>alert('Validacion Incorrecta'); window.location.href='Validar.html';</script>";
+  echo "<script type='text/javascript'>alert('Validacion Incorrecta'); window.location.href='validar.html';</script>";
   //header('Location:Validar.html');
 }
 
--- rlogin.php
+++ rlogin.php
@@ -3,18 +3,13 @@ session_start();
 include 'Base.php';
 //Coneccion ala base de datos
 $conn = conectar();
-if($conn->connect_error){
-die("Conexion a la base fallida: " . $connect_error);
-}
 //Obtencion de los datos de los campos
- $cedula = $_POST['cedula'];
-  $contra = $_POST['contra'];
+$cedula = $_POST['cedula'];
+$contra = $_POST['contra'];
 
 
 //Llamado al procedimiento almacenado
-//$sql = "CALL Login (".$cedula .", '".$contra. "', @Rol2)";
 $sql ="CALL Login(".$cedula.",'".$contra."');";
-//Puede implicar fallos no probado
 $respuesta = $conn->query($sql);
 if($respuesta->num_rows >0){
   $fila = $respuesta->fetch_assoc();
@@ -23,23 +18,7 @@ if($respuesta->num_rows >0){
   $_SESSION['usuario'] = $cedula;
   $_SESSION['Rol'] = $Rol;
   echo "<script type='text/javascript'>alert('Login Exitoso'); window.location.href = 'index.php';</script>";
-  //header('Location:index.php');
 } else {
   echo "<script type='text/javascript'>alert('Login Fallido'); window.location.href= 'login.html';</script>";
-  //header('Location:login.html');
 }
-//Se determinal la variable de sesion rol para mas adelante
-
-//Se determinar si el login fue exitoso si rol tiene un valor mayor a 0
-/*
-if($Rol = 0)
-{
-  echo "<script type='text/javascript'>alert(Login Fallido);</script>";
-  header('Location:login.php');
-}
-else {
-  echo "<script type='text/javascript'>alert(Login Exitoso);</script>";
-  header('Location:index.php');
-}*/
-
 ?>
